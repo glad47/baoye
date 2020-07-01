9709c197103d8432266c76862905bde0077dd334
zl <heimazhoulei@163.com>
2020-07-01 10:19:37 +0800

    地址修改

--- apps/view/src/SpecificationInput/AjaxService.ts
+++ apps/view/src/SpecificationInput/AjaxService.ts
@@ -1,30 +1,30 @@
 import Axios from "axios";
 
 //线上
-export const baseUrl = "https://www.pcbonline.com/"
-export const gerberUploadUrl = "http://47.89.176.26:8888/";
+// export const baseUrl = "https://www.pcbonline.com/"
+// export const gerberUploadUrl = "http://47.89.176.26:8888/";
 //线下
-// export const baseUrl = "http://192.168.0.181:8871/"
-// export const gerberUploadUrl = "http://192.168.0.181:8888/";
+export const baseUrl = "http://192.168.0.181:8871/"
+export const gerberUploadUrl = "http://192.168.0.181:8888/";
 
 // export function ajaxBuildTime (){
 //     return Axios.get(baseUrl+ 'quote/getBuildTime')
 // }
 
 export const ajaxBuildTime = (data: any) =>{
-    return Axios.get(baseUrl + `quote/getBuildTime?areaSq=${data.areaSq}&layerNum=${data.layerNum}`);
+    return Axios.get(baseUrl + `api/getBuildTime?areaSq=${data.areaSq}&layerNum=${data.layerNum}`);
 }
 
 export const ajaxSubtotal = (data: any) =>{
     return Axios.request({
-        url: baseUrl+'quote/countAdditionInfoV2',
+        url: baseUrl+'api/countAdditionInfoV2',
         method: 'post',
         data: data
     })
 }
 
 export const fetchShipingCost = (data: any) =>{
-    return Axios.get(baseUrl + `quote/getShippingCost?courierId=1&countryId=${data.countryId}&totalWeight=${data.totalWeight}`);
+    return Axios.get(baseUrl + `api/getShippingCost?courierId=1&countryId=${data.countryId}&totalWeight=${data.totalWeight}`);
 }
 
 export const ajaxAddQuote = (data: any) => {
@@ -37,7 +37,7 @@ export const ajaxAddQuote = (data: any) => {
 
 export const ajaxAssemblyCast = (data: any)=>{
     console.log(data);
-    return Axios.get(baseUrl + `quote/getAssemblyQuote?assemblyType=${data.assemblyType}&uniquePartNum=${data.uniquePartNum}&smtPartNum=${data.smtPartNum}&throughHolePartNum=${data.throughHolePartNum}&assemblySide=${data.assemblySide}&quantity=${data.quantity}`);
+    return Axios.get(baseUrl + `api/getAssemblyQuote?assemblyType=${data.assemblyType}&uniquePartNum=${data.uniquePartNum}&smtPartNum=${data.smtPartNum}&throughHolePartNum=${data.throughHolePartNum}&assemblySide=${data.assemblySide}&quantity=${data.quantity}`);
 }
 
 export const ajaxFileUpload = (file: File[]) =>{
@@ -48,6 +48,6 @@ export const ajaxFileUpload = (file: File[]) =>{
         headers:{'Content-Type': 'multipart/form-data',},
         method: 'post',
         data: fromData,
-        url: baseUrl + 'quote/upload'
+        url: baseUrl + 'api/quote/upload'
     })
 }
