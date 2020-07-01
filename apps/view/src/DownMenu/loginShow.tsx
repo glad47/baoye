1cc238d298eb102fdecab2a3152ed73bd2a36542
sky <1668541815@qq.com>
2020-07-01 11:24:14 +0800

    login doen

--- apps/view/src/DownMenu/loginShow.tsx
+++ apps/view/src/DownMenu/loginShow.tsx
@@ -6,10 +6,10 @@ export default class loginShow extends Component {
             <div className="login_show">
                 <p className='down_sw_login'></p>
                 <div className='login_nav_two'>
-                    <a >Change the password</a>
-                    <a >My information</a>
-                    <a ><ShoppingCartOutlined />  My order</a>
-                    <a ><PoweroffOutlined/>    Login out</a>
+                    <a href='/user/resetPwd'>Change the password</a>
+                    <a href='#1'>My information</a>
+                    <a href='/awaitingAudit'><ShoppingCartOutlined />  My order</a>
+                    <a href='#loginOut'><PoweroffOutlined/>    Login out</a>
                 </div>
             </div>
         )
