a3a6d3278de48f1c9bf6e38d4c248014d1144adf
sky <1668541815@qq.com>
2020-07-03 10:00:22 +0800

    移除不相干图片

--- apps/view/src/SpecificationInput/GerberShow.tsx
+++ apps/view/src/SpecificationInput/GerberShow.tsx
@@ -9,16 +9,16 @@ const GerberShow: React.FC<GerberShowProps> = (props) =>{
     const {loading,pcbSizeField:{singleSize:{sizeX}},svg,isShow,singleCopper,isBackToUpload,pcbStandardField,dispatch} = useAppState()
     const SIZE_CLASS_NAME = sizeX && sizeX > 70 ? 'vertical_svg_first' : 'transverse_svg_first'
     const SIZE_FIRST_CLASS_NAME = sizeX && sizeX > 70 ? 'vertical_svg' : 'transverse_svg'
-    let [imgSrc,setImgSrc]=useState(String)
+    //let [imgSrc,setImgSrc]=useState(String)
     const aginUpload=()=>{
         dispatch(backToUpload(true))
     }
-    useEffect(() => {
-        const {material,layer,silkscreen,solderMask}=pcbStandardField
-        let newLayer=layer.substr(0,1)
-        let defalutImgSrc=require(`../images/diagram/${material}${newLayer}${solderMask}${silkscreen}.png`)
-        setImgSrc(defalutImgSrc)
-    }, [])
+    // useEffect(() => {
+    //     const {material,layer,silkscreen,solderMask}=pcbStandardField
+    //     let newLayer=layer.substr(0,1)
+    //     let defalutImgSrc=require(`../images/diagram/${material}${newLayer}${solderMask}${silkscreen}.png`)
+    //     setImgSrc(defalutImgSrc)
+    // }, [])
         return(
             <>
             { isShow? 
@@ -45,7 +45,7 @@ const GerberShow: React.FC<GerberShowProps> = (props) =>{
                     </>
                     :
                     <div className='show_default_img'>
-                        <img src={imgSrc}/>
+                        <img src={require('../images/FR42greenwhite.png')}/>
                     </div>
                 }     
                 </>
