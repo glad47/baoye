330f73bee0c673d9bb31ec855451f931deed9f3f
sky <1668541815@qq.com>
2020-07-08 12:40:47 +0800

    修改图片

--- apps/view/src/SpecificationInput/GerberShow.tsx
+++ apps/view/src/SpecificationInput/GerberShow.tsx
@@ -7,11 +7,12 @@ interface GerberShowProps { }
 const WRAPPER_STYLE = 'absolute absolute--center near-black tc'
 //gerber显示组件
 const GerberShow: React.FC<GerberShowProps> = (props) => {
-    const { loading, pcbSizeField: { singleSize: { sizeX, sizeY } }, svg, isShow, singleCopper, isBackToUpload, pcbStandardField, dispatch, fillData } = useAppState()
+    const { loading, pcbSizeField: { singleSize: { sizeX, sizeY } }, svg, isShow, singleCopper, isBackToUpload, pcbStandardField, dispatch,allKeys } = useAppState()
     const SIZE_CLASS_NAME = sizeX && sizeY && sizeX > sizeY ? 'vertical_svg_first' : 'transverse_svg_first'
     const SIZE_FIRST_CLASS_NAME = sizeX && sizeY && sizeX > sizeY ? 'vertical_svg' : 'transverse_svg'
     const [isTimeOut, setTimer] = useState(false)
     const wordTip = !isTimeOut ? 'Successful geber file upload ！ Analyzing data, please wait and then check.' : 'It takes a little time for analyzing the file. You can also input by your own to get a quote.'
+    const {material,layer,silkscreen,solderMask}=pcbStandardField
     useEffect(() => {
         let timer: any
         let isComplete = new Promise((resolve, reject) => {
@@ -33,6 +34,17 @@ const GerberShow: React.FC<GerberShowProps> = (props) => {
             }
         }
     }, [])
+    let newLayer=layer.substr(0,1)
+    let defaultImgSrc
+    if(material !=='FR4' || newLayer !=='2' || solderMask !=='green' || silkscreen !=='white'){
+        if(solderMask!==silkscreen){
+            defaultImgSrc=require(`../images/diagram/${material}${newLayer}${solderMask}${silkscreen}.png`)
+        }else{
+            defaultImgSrc=require(`../images/diagram/${material}${newLayer}green${silkscreen}.png`)
+        }
+    }else{
+        defaultImgSrc=require('../images/FR42greenwhite.png')
+    }
     return (
         <>
             {svg !== null ?
@@ -59,7 +71,7 @@ const GerberShow: React.FC<GerberShowProps> = (props) => {
                         </>
                         :
                         <div className='show_default_img'>
-                            <img src={require('../images/FR42greenwhite.png')} />
+                            <img src={defaultImgSrc} />
                             <p className='failure_word'>Successful geber file upload but failed to analyze due to different file format. Please input by your own to get a quote.</p>
                         </div>
                     }
