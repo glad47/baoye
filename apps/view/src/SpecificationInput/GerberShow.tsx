b35996c643e7dc1402b9db4be5be978ffbd9ddc0
miracle <1668541815@qq.com>
2020-07-16 14:08:40 +0800

    done

--- apps/view/src/SpecificationInput/GerberShow.tsx
+++ apps/view/src/SpecificationInput/GerberShow.tsx
@@ -11,7 +11,7 @@ const GerberShow: React.FC<GerberShowProps> = (props) => {
     const SIZE_CLASS_NAME = sizeX && sizeY && sizeX > sizeY ? 'vertical_svg_first' : 'transverse_svg_first'
     const SIZE_FIRST_CLASS_NAME = sizeX && sizeY && sizeX > sizeY ? 'vertical_svg' : 'transverse_svg'
     const [isTimeOut, setTimer] = useState(false)
-    const wordTip = !isTimeOut ? 'Successful geber file upload ！ Analyzing data, please wait and then check.' : 'It takes a little time for analyzing the file. You can also input by your own to get a quote.'
+    const wordTip = isBackToUpload ? 'Successful geber file upload ！ Analyzing data, please wait and then check.' : 'It takes a little time for analyzing the file. You can also input by your own to get a quote.'
     const {material,layer,silkscreen,solderMask}=pcbStandardField
     useEffect(() => {
         let timer: any
