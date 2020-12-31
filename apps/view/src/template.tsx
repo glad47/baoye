9c1fe29b8270c0a6007cf6dbf91e9009b782a70d
miracle <1668541815@qq.com>
2020-12-31 10:35:19 +0800

    done

--- apps/view/src/template.tsx
+++ apps/view/src/template.tsx
@@ -9,45 +9,47 @@ import favicon16 from './images/favicon-16x16.png'
 import favicon32 from './images/favicon-32x32.png'
 import favicon64 from './images/favicon-64x64.png'
 import faviconNew from './images/logo2.ico'
+import { any } from 'core-js/fn/promise'
 
 type Props = {
-  htmlWebpackPlugin: {
-    options: {
-      title: string
-      description: string
-      author: string
+    htmlWebpackPlugin: {
+        options: {
+            title: string
+            description: string
+            author: string
+        }
     }
-  }
 }
 
 export default function StaticTemplate(props: Props): string {
-  const { options } = props.htmlWebpackPlugin
+    const { options } = props.htmlWebpackPlugin
 
-  return `<!doctype html>${ReactDomServer.renderToStaticMarkup(
-    <html lang="en" className="h-100 lh-solid">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="description" content={options.description} />
-        <meta name="author" content={options.author} />
-        <title>{options.title}</title>
-        <link rel="shortcut icon" href={faviconNew} />
-        <link rel="icon" type="image/png" sizes="16x16" href={faviconNew} />
-        <link rel="icon" type="image/png" sizes="32x32" href={faviconNew} />
-        <link rel="icon" type="image/png" sizes="32x32" href={faviconNew} />
-        <style>{faDom.css()}</style>
-        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3V6Y7YZNEE"></script>
-      </head>
-      <body className="h-100 overflow-hidden">
-        <div
-          data-hook="root"
-          className="h-100"
-          dangerouslySetInnerHTML={{
-            __html: ReactDomServer.renderToString(<App />),
-          }}
-        />
-      </body>
-    </html>
-  )}`
+    return `<!doctype html>${ReactDomServer.renderToStaticMarkup(
+        <html lang="en" className="h-100 lh-solid">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <meta name="description" content={options.description} />
+                <meta name="author" content={options.author} />
+                <title>{options.title}</title>
+                <link rel="shortcut icon" href={faviconNew} />
+                <link rel="icon" type="image/png" sizes="16x16" href={faviconNew} />
+                <link rel="icon" type="image/png" sizes="32x32" href={faviconNew} />
+                <link rel="icon" type="image/png" sizes="32x32" href={faviconNew} />
+                <style>{faDom.css()}</style>
+                <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-3V6Y7YZNEE"></script>
+                <script type="text/javascript" src='./Analysis/ga.js'> </script>
+            </head>
+            <body className="h-100 overflow-hidden">
+                <div
+                    data-hook="root"
+                    className="h-100"
+                    dangerouslySetInnerHTML={{
+                        __html: ReactDomServer.renderToString(<App />),
+                    }}
+                />
+            </body>
+        </html>
+    )}`
 }
